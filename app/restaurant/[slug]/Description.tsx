f57e45da0742cfd4eaefe54bd9d9ba1519df9d9e
sahildhimandesigner <sahil@founderandlightning.com>
2023-08-09 15:10:49 +0530

    created the description and title component

---
+++ app/restaurant/[slug]/Description.tsx
@@ -0,0 +1,50 @@
+const Description = () => {
+    return(
+        <>
+            <div className="mt-4">
+                <p className="text-lg font-light">
+                    The classics you love prepared with a perfect twist, all served up
+                    in an atmosphere that feels just right. That’s the Milestones
+                    promise. So, whether you’re celebrating a milestone, making the most
+                    of Happy Hour or enjoying brunch with friends, you can be sure that
+                    every Milestones experience is a simple and perfectly memorable one.
+                </p>
+                </div>
+                {/* DESCRIPTION */} {/* IMAGES */}
+                <div>
+                <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
+                    5 photos
+                </h1>
+                <div className="flex flex-wrap">
+                    <img
+                    className="w-56 h-44 mr-1 mb-1"
+                    src="https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpg"
+                    alt=""
+                    />
+                    <img
+                    className="w-56 h-44 mr-1 mb-1"
+                    src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701450.jpg"
+                    alt=""
+                    />
+                    <img
+                    className="w-56 h-44 mr-1 mb-1"
+                    src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701452.jpg"
+                    alt=""
+                    />
+                    <img
+                    className="w-56 h-44 mr-1 mb-1"
+                    src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701453.jpg"
+                    alt=""
+                    />
+                    <img
+                    className="w-56 h-44 mr-1 mb-1"
+                    src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701454.jpg"
+                    alt=""
+                    />
+                </div>
+            </div>
+        </>  
+    );
+}
+
+export default Description;
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 import NavBar from "../../component/NavBar";
 import Header from "./components/Header";
 import RestaurantNavBar from "./components/RestaurantNavBar";
+import Title from "./Title";
+import Rating from "./Rating";
+import Description from "./Description";
 
 export default function RestaurantDetails(){
     return(
@@ -17,62 +20,11 @@ export default function RestaurantDetails(){
                         {/* RESAURANT NAVBAR */}
                         <RestaurantNavBar />
                         {/* RESAURANT NAVBAR */} {/* TITLE */}
-                        <div className="mt-4 border-b pb-6">
-                            <h1 className="font-bold text-6xl">Milesstone Grill</h1>
-                        </div>
+                        <Title />
                         {/* TITLE */} {/* RATING */}
-                        <div className="flex items-end">
-                            <div className="ratings mt-2 flex items-center">
-                                <p>*****</p>
-                                <p className="text-reg ml-3">4.9</p>
-                            </div>
-                            <div>
-                                <p className="text-reg ml-4">600 Reviews</p>
-                            </div>
-                        </div>
+                        <Rating />
                         {/* RATING */} {/* DESCRIPTION */}
-                            <div className="mt-4">
-                                <p className="text-lg font-light">
-                                    The classics you love prepared with a perfect twist, all served up
-                                    in an atmosphere that feels just right. That’s the Milestones
-                                    promise. So, whether you’re celebrating a milestone, making the most
-                                    of Happy Hour or enjoying brunch with friends, you can be sure that
-                                    every Milestones experience is a simple and perfectly memorable one.
-                                </p>
-                                </div>
-                                {/* DESCRIPTION */} {/* IMAGES */}
-                                <div>
-                                <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
-                                    5 photos
-                                </h1>
-                                <div className="flex flex-wrap">
-                                    <img
-                                    className="w-56 h-44 mr-1 mb-1"
-                                    src="https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpg"
-                                    alt=""
-                                    />
-                                    <img
-                                    className="w-56 h-44 mr-1 mb-1"
-                                    src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701450.jpg"
-                                    alt=""
-                                    />
-                                    <img
-                                    className="w-56 h-44 mr-1 mb-1"
-                                    src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701452.jpg"
-                                    alt=""
-                                    />
-                                    <img
-                                    className="w-56 h-44 mr-1 mb-1"
-                                    src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701453.jpg"
-                                    alt=""
-                                    />
-                                    <img
-                                    className="w-56 h-44 mr-1 mb-1"
-                                    src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701454.jpg"
-                                    alt=""
-                                    />
-                                </div>
-                            </div>
+                        <Description />
                         {/* IMAGES */} {/* REVIEWS */}
                         <div>
                             <h1 className="font-bold text-3xl mt-10 mb-7 borber-b pb-5">
