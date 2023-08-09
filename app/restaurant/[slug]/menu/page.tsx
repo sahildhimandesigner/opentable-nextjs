67dcabbca8318a6c708e53533e685d14e22ec5ea
sahildhimandesigner <sahil@founderandlightning.com>
2023-08-09 16:16:09 +0530

    created the component for reservation form and menu

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,6 +1,7 @@
-import Link from "next/link";
 import NavBar from "../../../component/NavBar";
 import Header from "../../../component/Header";
+import RestaurantNavBar from "../components/RestaurantNavBar";
+import Menu from "../Menu";
 
 const RestaurantMenuPage = () => {
     return(
@@ -14,29 +15,9 @@ const RestaurantMenuPage = () => {
             <div className="flex m-auto w-2/3 text-black justify-between items-start 0 -mt-11">
               <div className="bg-white w-[100%] rounded p-3 shadow">
                 {/* RESAURANT NAVBAR */}
-                <nav className="flex text-reg border-b pb-2">
-                  <Link href="/restaurant/milestones-grill" className="mr-7"> Overview </Link>
-                  <Link href="/restaurant/milestones-grill/menu" className="mr-7"> Menu </Link>
-                </nav>
+                <RestaurantNavBar />
                 {/* RESAURANT NAVBAR */} {/* MENU */}
-                <main className="bg-white mt-5">
-                  <div>
-                    <div className="mt-4 pb-1 mb-1">
-                      <h1 className="font-bold text-4xl">Menu</h1>
-                    </div>
-                    <div className="flex flex-wrap justify-between">
-                      {/* MENU CARD */}
-                      <div className="border rounded p-3 w-[49%] mb-3">
-                        <h3 className="font-bold text-lg">Surf and Turf</h3>
-                        <p className="font-light mt-1 text-sm">
-                          A well done steak with lobster and rice
-                        </p>
-                        <p className="mt-7">$80.00</p>
-                      </div>
-                      {/* MENU CARD */}
-                    </div>
-                  </div>
-                </main>
+                <Menu />
                 {/* MENU */}
               </div>
             </div>
