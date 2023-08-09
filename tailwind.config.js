d5f1cda74966b39744a27df73e2137294f666ff7
sahildhimandesigner <sahil@founderandlightning.com>
2023-08-09 10:38:37 +0530

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontSize: {
+      "7xl": "85px"
+    },
+  },
+  plugins: [],
+}
