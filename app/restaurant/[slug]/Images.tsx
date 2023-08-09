451405c6b0ca2d39ca9af4a314c86535793a4efd
sahildhimandesigner <sahil@founderandlightning.com>
2023-08-09 15:23:42 +0530

    created the reservation card component

---
+++ app/restaurant/[slug]/Images.tsx
@@ -0,0 +1,33 @@
+const Images = () => {
+    return(
+        <div className="flex flex-wrap">
+            <img
+            className="w-56 h-44 mr-1 mb-1"
+            src="https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpg"
+            alt=""
+            />
+            <img
+            className="w-56 h-44 mr-1 mb-1"
+            src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701450.jpg"
+            alt=""
+            />
+            <img
+            className="w-56 h-44 mr-1 mb-1"
+            src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701452.jpg"
+            alt=""
+            />
+            <img
+            className="w-56 h-44 mr-1 mb-1"
+            src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701453.jpg"
+            alt=""
+            />
+            <img
+            className="w-56 h-44 mr-1 mb-1"
+            src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701454.jpg"
+            alt=""
+            />
+        </div>
+    )
+}
+
+export default Images;
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -5,6 +5,9 @@ import RestaurantNavBar from "./components/RestaurantNavBar";
 import Title from "./Title";
 import Rating from "./Rating";
 import Description from "./Description";
+import Images from "./Images";
+import Reviews from "./Reviews";
+import ReservationCard from "./ReservationCard";
 
 export default function RestaurantDetails(){
     return(
@@ -17,87 +20,17 @@ export default function RestaurantDetails(){
                 {/* HEADER */} {/* DESCRIPTION PORTION */}
                 <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
                     <div className="bg-white w-[70%] text-black rounded p-3 shadow">
-                        {/* RESAURANT NAVBAR */}
                         <RestaurantNavBar />
-                        {/* RESAURANT NAVBAR */} {/* TITLE */}
                         <Title />
-                        {/* TITLE */} {/* RATING */}
                         <Rating />
-                        {/* RATING */} {/* DESCRIPTION */}
                         <Description />
-                        {/* IMAGES */} {/* REVIEWS */}
-                        <div>
-                            <h1 className="font-bold text-3xl mt-10 mb-7 borber-b pb-5">
-                                What 100 people are saying
-                            </h1>
-                        <div>
-                            {/* REVIEW CARD */}
-                            <div className="border-b pb-7 mb-7">
-                                <div className="flex">
-                                    <div className="w-1/6 flex flex-col items-center">
-                                        <div
-                                            className="rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center"
-                                        >
-                                            <h2 className="text-white text-2xl">MJ</h2>
-                                        </div>
-                                        <p className="text-center">Micheal Jordan</p>
-                                    </div>
-                                    <div className="ml-10 w-5/6">
-                                        <div className="flex items-center">
-                                            <div className="flex mr-5">*****</div>
-                                        </div>
-                                        <div className="mt-5">
-                                            <p className="text-lg font-light">
-                                            Laurie was on top of everything! Slow night due to the
-                                            snow storm so it worked in our favor to have more one on
-                                            one with the staff. Delicious and well worth the money.
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* REVIEW CARD */}
-                        </div>
-                        </div>
-                        {/* REVIEWS */}
+                        <Images />
+                        <Reviews />
                     </div>
                     <div className="w-[27%] relative text-reg">
-                        <div className="fixed w-[15%] bg-white rounded p-3 shadow">
-                            <div className="text-center text-black border-b pb-2 font-bold">
-                                <h4 className="mr-7 text-lg">Make a Reservation</h4>
-                            </div>
-                            <div className="my-3 flex flex-col">
-                                <label className="text-black" htmlFor="">Party size</label>
-                                <select name="" className="py-3 border-b font-light" id="">
-                                <option value="">1 person</option>
-                                <option value="">2 people</option>
-                                </select>
-                            </div>
-                            <div className="flex justify-between">
-                                <div className="flex flex-col w-[48%]">
-                                <label className="text-black" htmlFor="">Date</label>
-                                <input type="text" className="py-3 border-b font-light w-28" />
-                                </div>
-                                <div className="flex flex-col w-[48%]">
-                                <label className="text-black" htmlFor="">Time</label>
-                                <select name="" id="" className="py-3 border-b font-light">
-                                    <option value="">7:30 AM</option>
-                                    <option value="">9:30 AM</option>
-                                </select>
-                                </div>
-                            </div>
-                            <div className="mt-5">
-                                <button
-                                className="bg-red-600 rounded w-full px-4 text-white font-bold h-16"
-                                >
-                                Find a Time
-                                </button>
-                            </div>
-                        </div>
+                        <ReservationCard />
                     </div>
                 </div>
-                {/* DESCRIPTION PORTION */} {/* RESERVATION CARD PORTION */} {/* RESERVATION
-                CARD PORTION */}
             </main>
         </main>
     )
